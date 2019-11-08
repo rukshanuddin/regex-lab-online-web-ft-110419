6cdf6cb27fd3c9726f74c2c6657f41d457047a37
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-08 15:08:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
